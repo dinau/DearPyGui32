6549d6bc5b83c1f73e858890ed71b826d76551b4
dinau <audgit@gmail.com>
2024-05-13 00:54:55 +0900

    Change *.zip file to *.whl file

--- build_win32.bat
+++ build_win32.bat
@@ -1,9 +1,13 @@
 @echo off
 rem Using Python 3.11.9
+rem Install wheel package for 'bdist_wheel' option.
+rem pip install wheel
 
 set plat_name=win32
-python -m setup bdist --plat-name %plat_name% --dist-dir dist
+python -m setup bdist_wheel --plat-name %plat_name% --dist-dir dist
 
-copy win32\setup\setup.py output
-pip install -e output
+set VERSION=1.11.0-cp311-cp311-win32
+
+copy dist\dearpygui-%VERSION%.whl win32\dist
+pip install win32\dist\dearpygui-%VERSION%.whl
 python win32\tutorial_demo.py
--- win32/dist/INSTALL.md
+++ win32/dist/INSTALL.md
@@ -1,5 +1,5 @@
 DearPyGui for Win32 can be installed,
 
 ```sh
-pip install dearpygui-1.11.0.win32.zip
+pip install dearpygui-1.11.0-cp311-cp311-win32.whl
 ```
